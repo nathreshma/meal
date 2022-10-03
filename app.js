7e4cac91906619f8902ed85be972bf7ab1fc4eae
reshmanath <90786988+nathreshma@users.noreply.github.com>
2022-10-03 19:09:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// ========================GLOBAL VARIABLES======================================= //
+
 const main = document.querySelector("main")
 const favoriteRecipes = []
 const searchIngredients = []
 
-// =============================== NAVBAR ======================================== //
+
 const home = document.querySelector("#nav-home")
 home.addEventListener("click", loadHome)
 const saved = document.querySelector("#nav-save")
@@ -11,11 +11,11 @@ saved.addEventListener("click", function () {
   viewRecipeBox(favoriteRecipes, searchIngredients)
 })
 
-// =============================== HEADER ======================================== //
+
 const mainHeader = document.querySelector(".header-h1")
 mainHeader.addEventListener("click", loadHome)
 
-// Event listener for the search bar
+
 const form = document.querySelector("form")
 form.addEventListener("submit", (e) => {
   e.preventDefault()
@@ -26,7 +26,7 @@ form.addEventListener("submit", (e) => {
 })
 
 
-// =============================== HOME SCREEN ==================================== //
+
 loadHome()
 
 async function loadHome() {
@@ -49,7 +49,6 @@ async function loadHome() {
   }
 }
 
-// =============================== SEARCH SCREEN ================================ //
 async function showResults(ingredient) {
   // Store the URL that accesses the API in a variable
   const ingredientURL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
@@ -63,25 +62,25 @@ async function showResults(ingredient) {
       main.append(noRecipes)
       return
     }
-    // Report the number of results found
+    
     if (recipes.length > 0) {
       const recipeCount = countRecipes(recipes, ingredient)
       main.append(recipeCount)
     }
     recipes.forEach(recipe => {
-      // Create a container for each image and dish
+      
       const listRecipeDiv = document.createElement("div")
       listRecipeDiv.className = "list-recipe-div"
       main.append(listRecipeDiv)
       
-      // Create html elements for each image and dish with attributes
+      
       const image = createListImage(recipe)
       const dish = createListDish(recipe)
       listRecipeDiv.addEventListener("click", (e) => {
         renderRecipe(e.target.id, ingredient)
       })
       
-      // Create a container for image and dish, append to DOM
+     
       listRecipeDiv.append(image)
       listRecipeDiv.append(dish)
     })
@@ -92,8 +91,7 @@ async function showResults(ingredient) {
   }
 }
 
-// =============================== RECIPE SCREEN ================================ //
-// Display the photo, ingredients, and instructions for a specific recipe
+
 async function renderRecipe(id, ingredient) {
   removeMain()
   const recipeURL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
@@ -111,10 +109,10 @@ async function renderRecipe(id, ingredient) {
     main.append(recipeDiv)
     recipeDiv.append(image)
 
-    // Append ingredients within this function
+    
     listIngredients(recipe)
 
-    // List instructions
+    
     const instructionsDiv = document.createElement("div")
     instructionsDiv.className = "instructions-div"
     main.append(instructionsDiv)
@@ -123,12 +121,12 @@ async function renderRecipe(id, ingredient) {
     instructionsDiv.append(instructionsHeader)
     instructionsDiv.append(instructions)
     
-    // Create a recipe button div and prepend to the main part
+    
     const recipeButtonDiv = document.createElement("div")
     recipeButtonDiv.className = "recipe-button-div"
     main.prepend(recipeButtonDiv)
 
-    // Append back/explore button
+    
     const backExplore = createBackExplore()
     let randomFlag = window.localStorage.getItem("randomRecipe")
     let exploreFlag = window.localStorage.getItem("exploreRecipe")
@@ -158,9 +156,9 @@ async function renderRecipe(id, ingredient) {
   }
 }
 
-// Keep track of saved recipes
+
 function saveRecipe(id, searchIngredient) {
-  // Check for duplicate recipes
+  
   if (favoriteRecipes.includes(id)) {
     viewRecipeBox(favoriteRecipes, searchIngredients)
   } else {
@@ -171,7 +169,7 @@ function saveRecipe(id, searchIngredient) {
   }
 }
 
-// =============================== EXPLORE SCREEN ================================ //
+
 async function exploreMore(category) {
   removeMain()
   main.removeAttribute("id")
@@ -190,7 +188,7 @@ async function exploreMore(category) {
       listRecipeDiv.className = "list-recipe-div"
       main.append(listRecipeDiv)
     
-    // Create html elements for each image and dish with attributes
+    
       const image = createListImage(recipe)
       const dish = createListDish(recipe)
       ingredient = recipe.strIngredient1
@@ -199,7 +197,7 @@ async function exploreMore(category) {
         window.localStorage.setItem("exploreRecipe", e.target.id)
       })
     
-    // Create a container for image and dish, append to DOM
+    
     listRecipeDiv.append(image)
     listRecipeDiv.append(dish)
   })
@@ -210,12 +208,12 @@ async function exploreMore(category) {
   }
 }
 
-// =============================== SAVED SCREEN ================================ //
+
 async function viewRecipeBox(recipes, ingredients) {
   removeMain()
   main.removeAttribute("id");
 
-  // Create an array for recipe URLs
+ 
   let recipeURLs = []
   for (let i = 0; i < recipes.length; i++) {
     recipeURLs.push(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipes[i]}`)
@@ -226,7 +224,7 @@ async function viewRecipeBox(recipes, ingredients) {
       let response = await axios.get(recipeURLs[i])
       let recipe = response.data.meals[0]
       
-      // Create a div for each recipe and store image and name
+     
       const listRecipeDiv = document.createElement("div")
       listRecipeDiv.className = "list-recipe-div"
       main.append(listRecipeDiv)
@@ -256,7 +254,7 @@ async function viewRecipeBox(recipes, ingredients) {
       recipeIDs.push(image.id)
     }
     
-    // Indicate how many recipes are saved at the top of main
+    
     const savedRecipesHeader = createSavedHeader(recipes)
     main.prepend(savedRecipesHeader)
     
